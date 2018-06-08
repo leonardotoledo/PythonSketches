5077303164cc4ae0e142bdf4b7d25f775dedc7d1
Leonardo T. Ferreira <leonardotoledo@outlook.com>
2018-06-07 21:35:56 -0300

    Created script for CD Py meetings

--- sorteioGUI.py
+++ sorteioGUI.py
@@ -2,7 +2,7 @@ from tkinter import *
 from myRepo import sort
 
 def sortear():
-   membros = [e1.get(), e2.get(), e3.get(), e4.get(), e5.get(), e6.get()]
+   membros = [membro1.get(), membro2.get(), membro3.get(), membro4.get(), membro5.get(), membro6.get()]
    sort(membros)
 
 master = Tk()
@@ -13,21 +13,21 @@ Label(master, text="Membro 4: ").grid(row=3)
 Label(master, text="Membro 5: ").grid(row=4)
 Label(master, text="Membro 6: ").grid(row=5)
 
-e1 = Entry(master)
-e2 = Entry(master)
-e3 = Entry(master)
-e4 = Entry(master)
-e5 = Entry(master)
-e6 = Entry(master)
+membro1 = Entry(master)
+membro2 = Entry(master)
+membro3 = Entry(master)
+membro4 = Entry(master)
+membro5 = Entry(master)
+membro6 = Entry(master)
 
-e1.grid(row=0, column=1)
-e2.grid(row=1, column=1)
-e3.grid(row=2, column=1)
-e4.grid(row=3, column=1)
-e5.grid(row=4, column=1)
-e6.grid(row=5, column=1)
+membro1.grid(row=0, column=1)
+membro2.grid(row=1, column=1)
+membro3.grid(row=2, column=1)
+membro4.grid(row=3, column=1)
+membro5.grid(row=4, column=1)
+membro6.grid(row=5, column=1)
 
 Button(master, text='Sair', command=master.quit).grid(row=6, column=0, sticky=W, pady=4)
 Button(master, text='Fazer sorteio', command=sortear).grid(row=6, column=1, sticky=W, pady=4)
 
-mainloop( )
+mainloop()
