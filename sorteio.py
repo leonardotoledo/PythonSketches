2467c42a97d685b1555006c9e47d538f4dfd5382
Leonardo Tolêdo <odelotodranoel@gmail.com>
2018-06-17 22:07:34 -0300

    Better version of the GUI App

--- sorteio.py
+++ sorteio.py
@@ -5,57 +5,46 @@ def sort(members):
 
     aux=[]
     aux.append("Tempo total da rodada: %d min\n" %(len(members)*10))
+    rotulo = []
+    rotulo.append(Label(resultado, text=aux[0]).grid(row=0, column=0))
 
     shuffle(members)
 
     for i in range(1,len(members)):
         aux.append("\nDupla %d:\n Piloto: %s\n Co-piloto: %s\n" %(i,members[i-1],members[i]))
+        rotulo.append(Label(resultado, text=aux[i]).grid(row=i, column=0))
         if (i==len(members)-1) :
             aux.append("\nDupla %d:\n Piloto: %s\n Co-piloto: %s\n" %(len(members),members[len(members)-1],members[0]))
-
-    rotulo6['text'] = aux
+            rotulo.append(Label(resultado, text=aux[len(members)]).grid(row=len(members), column=0))
 
 
 def sortear():
-   membros = [membro1.get(), membro2.get(), membro3.get(), membro4.get(), membro5.get()]
+   membros = [member.get() for member in membro]
    sort(membros)
 
 # Instancia a janela:
 
 janela = Tk()
+resultado = Tk()
+
+# Numero de pessoas a sortear:
+n = 5
 
 # Instancia os rótulos usando a classe Label e os coloca na janela usando o gerenciador de layout 'grid'
-
-rotulo1 = Label(janela, text="Membro 1: ")
-rotulo1.grid(row=0, column=0)
-rotulo2 = Label(janela, text="Membro 2: ")
-rotulo2.grid(row=1, column=0)
-rotulo3 = Label(janela, text="Membro 3: ")
-rotulo3.grid(row=2, column=0)
-rotulo4 = Label(janela, text="Membro 4: ")
-rotulo4.grid(row=3, column=0)
-rotulo5 = Label(janela, text="Membro 5: ")
-rotulo5.grid(row=4, column=0)
-rotulo6 = Label(janela, text='')
-rotulo6.grid(row=6, column=1)
+rotulos=[]
+for i in range(n):
+    rotulos.append(Label(janela, text='Membro '+str(i+1)+': ').grid(row=i, column=0))
 
 # Usa a classe Entry para criar as caixas de texto e em seguida as coloca na janela com o gerenciador de layout 'grid'
 
-membro1 = Entry(janela)
-membro1.grid(row=0, column=1)
-membro2 = Entry(janela)
-membro2.grid(row=1, column=1)
-membro3 = Entry(janela)
-membro3.grid(row=2, column=1)
-membro4 = Entry(janela)
-membro4.grid(row=3, column=1)
-membro5 = Entry(janela)
-membro5.grid(row=4, column=1)
+membro = []
+for i in range(n):
+    membro.append(Entry(janela))
+    membro[i].grid(row=i, column=1)
 
-sairBotao = Button(janela, text='Sair', command=janela.quit)
-sairBotao.grid(row=5, column=0, sticky=W, pady=4)
-sortearBotao = Button(janela, text='Fazer sorteio', command=sortear)
-sortearBotao.grid(row=5, column=1, sticky=W, pady=4)
+sairBotao = Button(janela, text='Sair', command=janela.quit).grid(row=len(rotulos), column=0, sticky=W, pady=4)
+sortearBotao = Button(janela, text='Fazer sorteio', command=sortear).grid(row=len(rotulos), column=1, sticky=W, pady=4)
 
 janela.title('Sorteio CD Py')
+resultado.title('Resultado')
 janela.mainloop()
